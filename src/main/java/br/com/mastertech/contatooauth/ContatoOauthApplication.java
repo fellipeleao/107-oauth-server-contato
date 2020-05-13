38a58680bf249acc5f6225376cb25f57493f4ba4
a2 <a2@localhost.localdomain>
2020-05-13 12:07:56 -0300

    Arquivos Iniciais do Projeto

---
+++ src/main/java/br/com/mastertech/contatooauth/ContatoOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.mastertech.contatooauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatoOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatoOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mastertech/contatooauth/ContatoOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mastertech.contatooauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatoOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
